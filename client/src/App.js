3ed9954bbac042fa9e4cd589b1527c021690d128
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2024-02-06 03:27:43 -1200

    Fetched data from the database

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter , Route } from 'react-router-dom';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
 
       <Navbar/>
+
+      <BrowserRouter>
+      <Route path='/home' exact Component={Homescreen} />
+      </BrowserRouter>
     </div>
   );
 }
